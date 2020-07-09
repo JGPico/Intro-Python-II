a7758c9adf0391f4b96aa16f9843616a8dddfeb2
JGPico <minq_2@hotmail.com>
2020-07-08 18:10:23 -0600

    Player and Room classes added

--- src/adv.py
+++ src/adv.py
@@ -1,28 +1,29 @@
-# from room import Room
+from room import Room
+from player import Player
 
-# # Declare all the rooms
+# Declare all the rooms
 
-# room = {
-#     'outside':  Room("Outside Cave Entrance",
-#                      "North of you, the cave mount beckons"),
+room = {
+    'outside':  Room("The Outside Cave Entrance",
+                     "North of you, the cave mount beckons"),
 
-#     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-# passages run north and east."""),
+    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+passages run north and east."""),
 
-#     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-# into the darkness. Ahead to the north, a light flickers in
-# the distance, but there is no way across the chasm."""),
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+into the darkness. Ahead to the north, a light flickers in
+the distance, but there is no way across the chasm."""),
 
-#     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-# to north. The smell of gold permeates the air."""),
+    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+to north. The smell of gold permeates the air."""),
 
-#     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-# chamber! Sadly, it has already been completely emptied by
-# earlier adventurers. The only exit is to the south."""),
-# }
+    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+chamber! Sadly, it has already been completely emptied by
+earlier adventurers. The only exit is to the south."""),
+}
 
 
-# # Link rooms together
+# Link rooms together
 
 # room['outside'].n_to = room['foyer']
 # room['foyer'].s_to = room['outside']
@@ -44,18 +45,22 @@
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
-user = str(input("\n [q] Quit [c] Continue: ")).lower()
+
+user = str(input("\n [q] Quit [c] Continue: ")).lower().strip()
+adventurer = Player("Gibbons", room['outside'])
+
 while not user == 'q':
+    print()
+    print(adventurer.current_room)
+    print()
     if user == 'c':
-        print('You are playing the game')
-    else:
-        print('You decided not to continue')
+        print('\nYou are playing the game\n')
 
     print("Choose to play again or quit")
-    user = str(input("\n [q] Quit [c] Continue: ")).lower()
+    user = str(input("\n [q] Quit [c] Continue: ")).lower().strip()
 
 print("\n Game Over \n")
-#
+
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 #
